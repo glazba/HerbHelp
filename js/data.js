40483648a7635d623e071d0fe47bbd0cf949e052
glazba <szabitomi207@gmail.com>
2026-06-10 13:08:59 +0200

    Expand herb database

--- js/data.js
+++ js/data.js
@@ -48,5 +48,97 @@ const herbs = [
             "Lorem ipsum.",
         image:
             "assets/citromfu.jpg"
-    }
+    },
+
+    {
+        id: 4,
+        name: "Levendula",
+        symptoms: ["stressz", "álmatlanság", "alvászavar"],
+        description:
+            "A levendula nyugtató hatású gyógynövény, amely segíthet a stressz csökkentésében és a pihentető alvás elősegítésében.",
+        usage:
+            "Tea, illóolaj vagy fürdő formájában használható.",
+        warnings:
+            "Nagy mennyiségben fáradékonyságot okozhat.",
+        image:
+            "assets/levendula.jpg"
+    },
+    {
+        id: 5,
+        name: "Borsmenta",
+        symptoms: ["hasfájás", "emésztési panaszok", "hányinger"],
+        description:
+            "A borsmenta frissítő hatású gyógynövény, amelyet gyakran alkalmaznak emésztési problémák és hányinger enyhítésére.",
+        usage:
+            "Leggyakrabban tea formájában fogyasztják, de illóolajként is használható.",
+        warnings:
+            "Reflux esetén fokozhatja a tüneteket.",
+        image:
+            "assets/borsmenta.jpg"
+    },
+    {
+        id: 6,
+        name: "Csalán",
+        symptoms: ["allergia", "fáradtság", "izületi panaszok"],
+        description:
+            "A csalán vitaminokban és ásványi anyagokban gazdag növény, amelyet gyakran alkalmaznak allergiás tünetek és fáradékonyság esetén.",
+        usage:
+            "Tea vagy étrend-kiegészítő formájában fogyasztható.",
+        warnings:
+            "Vízhajtó hatása miatt fokozott folyadékbevitel ajánlott.",
+        image:
+            "assets/csalan.jpg"
+    },
+    {
+        id: 7,
+        name: "Gyömbér",
+        symptoms: ["megfázás", "torokfájás", "hányinger"],
+        description:
+            "A gyömbér gyulladáscsökkentő és immunerősítő hatásáról ismert gyógynövény.",
+        usage:
+            "Tea, friss gyökér vagy étrend-kiegészítő formájában fogyasztható.",
+        warnings:
+            "Véralvadásgátló gyógyszerek szedése esetén csak kis mértékben fogyasztható.",
+        image:
+            "assets/gyomber.jpg"
+    },
+    {
+        id: 8,
+        name: "Hársfa",
+        symptoms: ["megvázás", "köhögés", "stressz"],
+        description:
+            "A hársfavirág hagyományosan megfázás és köhögés esetén alkalmazott növény.",
+        usage:
+            "Leggyakrabban teaként fogyasztják.",
+        warnings:
+            "Hosszútávú, nagy mennyiségű fogyasztása nem ajánlott.",
+        image:
+            "assets/harsfa.jpg"
+    },
+    {
+        id: 9,
+        name: "Bodza",
+        symptoms: ["megfázás", "köhögés", "láz"],
+        description:
+            "A bodzavirág népszerú gyógynövény, amelyet légúti panaszok és megfázás esetén alkalmaznak.",
+        usage:
+            "Tea vagy szörp formájában fogyasztható.",
+        warnings:
+            "A nyers bogyók fogyasztása nem ajánlott.",
+        image:
+            "assets/bodza.jpg"
+    },
+    {
+        id: 10,
+        name: "Körömvirág",
+        symptoms: ["bőrirritáció", "sebek", "gyulladás"],
+        description:
+            "A körömvirág elsősorban külsőleg alkalmazott gyógynövény, amely segítheti a bőr regenerációját.",
+        usage:
+            "Krém, kenőcs vagy borogatás formájában használható.",
+        warnings:
+            "Fészekvirágzatú növényekre allergia esetén allergiás reakciót válthat ki.",
+        image:
+            "assets/koromvirag.jpg"
+    },
 ]
